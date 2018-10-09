39ed37fb505bd472caf13e447fdbaa4eb6b0d7c7
Stefan Palade <seanfto@gmail.com>
2018-10-09 22:13:56 +0300

    Makefile: creating a sane environment
    Adding a couple of default targets for make and an install script that will do
    the heavy lifting. Creating the stages for each tool I intend to track with this
    repository.

---
+++ Makefile
@@ -0,0 +1,10 @@
+install:
+	./install.sh
+
+update:
+	./install.sh update
+
+uninstall:
+	./install.sh uninstall
+
+# vim: noexpandtab ts=4 sw=4 sts=0
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+# -*- mode: sh -*-
